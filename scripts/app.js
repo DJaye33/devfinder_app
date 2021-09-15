df1219fe10dd466ee48a2a3ce8c560faf43f9289
Danny JeanLouis <dlouis3386@gmail.com>
2021-09-15 00:44:49 -0400

    add theme effect for search

--- scripts/app.js
+++ scripts/app.js
@@ -2,12 +2,16 @@ const setDarkTheme = document.querySelector(".header__theme");
 const headerHeading = document.querySelector(".header__heading");
 const body = document.querySelector("body");
 const setThemeLight = document.querySelector(".header__theme-light");
+const searchForm = document.querySelector(".search");
+const searchInput = document.querySelector(".search__input");
 
 setDarkTheme.addEventListener("click", (evt) => {
   evt.currentTarget.classList.add("header__theme--dark");
   headerHeading.classList.add("header__heading--dark");
   body.style.backgroundColor = "var(--very-dark-blue)";
   setThemeLight.classList.remove("header__theme-light--light");
+  searchForm.classList.add("search--dark");
+  searchInput.classList.add("search__input--dark");
 });
 
 setThemeLight.addEventListener("click", (evt) => {
@@ -15,4 +19,6 @@ setThemeLight.addEventListener("click", (evt) => {
   headerHeading.classList.remove("header__heading--dark");
   body.style.backgroundColor = "";
   setThemeLight.classList.add("header__theme-light--light");
+  searchForm.classList.remove("search--dark");
+  searchInput.classList.remove("search__input--dark");
 });
