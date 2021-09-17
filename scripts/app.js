85526a548862f6a3d309dc9c7ab857a3e5dcb700
Danny JeanLouis <dlouis3386@gmail.com>
2021-09-17 17:32:09 -0400

    add info header dark theme

--- scripts/app.js
+++ scripts/app.js
@@ -4,6 +4,9 @@ const body = document.querySelector("body");
 const setThemeLight = document.querySelector(".header__theme-light");
 const searchForm = document.querySelector(".search");
 const searchInput = document.querySelector(".search__input");
+const sectionInfo = document.querySelector(".info");
+const infoUsername = document.querySelector(".info__username");
+const infoJoined = document.querySelector(".info__joined");
 
 setDarkTheme.addEventListener("click", (evt) => {
   evt.currentTarget.classList.add("header__theme--dark");
@@ -12,6 +15,9 @@ setDarkTheme.addEventListener("click", (evt) => {
   setThemeLight.classList.remove("header__theme-light--light");
   searchForm.classList.add("search--dark");
   searchInput.classList.add("search__input--dark");
+  sectionInfo.classList.add("info--dark");
+  infoUsername.classList.add("info__username--dark");
+  infoJoined.classList.add("info__joined--dark");
 });
 
 setThemeLight.addEventListener("click", (evt) => {
@@ -21,4 +27,7 @@ setThemeLight.addEventListener("click", (evt) => {
   setThemeLight.classList.add("header__theme-light--light");
   searchForm.classList.remove("search--dark");
   searchInput.classList.remove("search__input--dark");
+  sectionInfo.classList.remove("info--dark");
+  infoUsername.classList.remove("info__username--dark");
+  infoJoined.classList.remove("info__joined--dark");
 });
